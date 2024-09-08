26ab04134b3bccd42f796d62706a6317fa2fd083
Jaime <jromo01@ucm.es>
2024-09-08 15:35:41 +0200

    Arregladas rutas de ficheros

--- Scripts/Isoperimetric.py
+++ Scripts/Isoperimetric.py
@@ -28,11 +28,9 @@ def compute_matrixA(n) :
     for i in range(n-1) :
         for j in range(n-1) :
             assert A[i][j] == -A[j][i]
-    print("Matriz A")
-    for i in A: print(i)
     return A
 
-# Genera la matrizB
+# Calcula la matriz B
 def compute_matrixB(n) :
     B = [[0 for j in range(n-1)] for i in range(n-1)]
     for x in range(n-1) :
@@ -45,8 +43,6 @@ def compute_matrixB(n) :
     for i in range(n-1) :
         for j in range(n-1) :
             assert B[i][j] == B[j][i]
-    print("Matriz B")
-    for i in B: print(i)
     return B
 
 
@@ -54,8 +50,6 @@ def compute_matrixB(n) :
 def generate_pencil(n) :
     A = compute_matrixA(n)
     B = compute_matrixB(n)
-    #print("Matriz B")
-    #for i in B: print(i)
     C = [[0 for j in range(2*n-2)] for i in range(2*n-2)]
     D = [[0 for j in range(2*n-2)] for i in range(2*n-2)]
     coefC = n / (4*comb[2*n-1][n])
@@ -70,30 +64,24 @@ def generate_pencil(n) :
         for y in range(n-1) :
             D[x][y] = coefD * B[x][y]
             D[x+n-1][y+n-1] = D[x][y]
-    #print("Matriz C")
-    #for i in C: print(i)
     return np.asmatrix(C), np.asmatrix(D)
     
-
-def save_curve(n, x, y, name) :
+# Guarda una curva solución del problema 
+def save_curve(n, x, y, path) :
     xt = np.transpose(x)
     yt = np.transpose(y)
     B = compute_matrixB(n)
     coef = n * (n-1) / comb[2*n-1][n]
     den = np.matmul(np.matmul(xt, B), x).item(0) + np.matmul(np.matmul(yt, B), y).item(0)
     den = math.sqrt(coef * den)
-    #print(den)
     px = [0] * (n-1)
     py = [0] * (n-1)
-    #print(x, y)
     for i in range(n-1) :
         px[i] = l * x[i].item(0) / den
         py[i] = l * y[i].item(0) / den
-    #print(px)
-    #print(py)
-
-    fname = name + str(n) + ".in"
-    file = open(fname, "w")
+   
+    fname = path + str(n) + ".in"
+    file = open(path, "w")
     file.write(str(n+1) + "\n")
     file.write("0 0\n")
     for i in range(n-1) :
@@ -101,37 +89,31 @@ def save_curve(n, x, y, name) :
     file.write("0 0\n")
     file.close()
 
+# Algoritmo que calcula la solución al problema reducido
 def solution(n, l) :
-    C, D = generate_pencil(n)
-    A = np.asmatrix(compute_matrixA(n))
-    B = np.asmatrix(compute_matrixB(n))
-    M = np.matmul(LA.inv(B), A)
-    eigval, eigvec = LA.eig(M)
-    eigval = eigval.imag
-    #print("Autovalor con el método2 =",max(eigval)/(4*(n-1)))
-    #print("Área máxima con el método2 =", max(eigval)/(4*(n-1))*l*l)
-    
+    C, D = generate_pencil(n)    
     M = np.matmul(LA.inv(D), C)
     eigval, eigvec = LA.eig(M)
-    #print(eigval)
+    # Hay que tomar la parte real por los errores de precisión
     eigval = eigval.real
     eigvec = eigvec.real
     maxeigval = np.max(eigval)
     maxid = np.argmax(eigval)
     area = maxeigval * l*l
     precision = 10
-    data = open("eigenvalarea.in", "a")
+    data = open("./Data/Solutions/eigenvalarea.in", "a")
     s = str(n) + " " + str(round(maxeigval, precision)) + " " + str(round(area, precision))
     data.write(s + "\n")
     data.close()
-   # print("Máximo autovalor =", maxeigval)
+    print("n =", n)
+    print("Máximo autovalor =", maxeigval)
     print("Área máxima =", area)
     
     z = eigvec[:,maxid]
     x, y = np.split(z, 2)
-    save_curve(n, x, y, "iso")
+    save_curve(n, x, y, "./Data/Iso/iso")
 
-
+# Algoritmo optimizad que calcula la solución al problema reducido
 def solution_optimized(n, l) :
     A = np.asmatrix(compute_matrixA(n))
     B = np.asmatrix(compute_matrixB(n))
@@ -146,21 +128,24 @@ def solution_optimized(n, l) :
     area = lbda * l*l
     print("Área máxima con la optimización:", area)
     precision = 10
-    data = open("eigenvalareaopti.in", "a")
+    data = open("./Data/Solutions/eigenvalareaopti.in", "a")
     s = str(n) + " " + str(round(lbda, precision)) + " " + str(round(area, precision))
     data.write(s + "\n")
     data.close()
     y = eigvec[:, minid]
     x = np.matmul(M, y) / math.sqrt(mu)
-    #print(y)
-    #print(x)
-    save_curve(n, x, y, "opt")
+    save_curve(n, x, y, "./Data/Opt/opt")
 
 
 
+
+###############################
+# LLAMAR A LO QUE SE NECESITE #
+###############################
+
+# ¡¡¡¡Cuidado con los append en los ficheros de la carpeta Solution. No se sobreescribe lo que hubiera antes!!!!!
 
 l = 2*math.pi
-solution(3,l)
-#for n in range(3, 41) :
-    #solution(n, l)
+for n in range(3, 41) :
+    solution(n, l)
     #solution_optimized(n, l)

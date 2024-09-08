a8e1f5df55862dbba758eecd419cb1835c00ae6e
Jaime <jromo01@ucm.es>
2024-09-08 14:48:06 +0200

    Más puntos aleatorios

--- Scripts/Generate.py
+++ Scripts/Generate.py
@@ -12,11 +12,12 @@ def generate_points(n, max, i) :
         y = random.uniform(0, max)
         points.append([x, y])
     
-    file_name = "random" + str(i) + ".in"
+    file_name = "./Data/Random/random" + str(i) + ".in"
     f = open(file_name, "w")
     f.write(str(n) + "\n")
     for p in points:
         f.write(str(p[0]) + " " + str(p[1]) + "\n")
     f.close()
 
-generate_points(50, 50.0, 2)
+for i in range(1, 11) :
+    generate_points(50, 50.0, i)
